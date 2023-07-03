08d96daa8d9147cf566836825a875f37ba059010
linosk <linakoskar@gmail.com>
2023-07-03 20:30:46 +0200

    Success, first endpoints created

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -42,9 +45,68 @@ func connectDatabase() *mongo.Client {
 	return client
 }
 
+func getCollection(client *mongo.Client, databaseName string, collectionName string) *mongo.Collection {
+
+	collection := client.Database(databaseName).Collection(collectionName)
+	return collection
+}
+
+var (
+	client     = connectDatabase()
+	collection = getCollection(client, "bookstore", "books")
+)
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome")
+	fmt.Println("Hompeage")
+}
+
+type Book struct {
+	Title  string
+	Author string
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+
+	findOptions := options.Find()
+	cursor, err := collection.Find(ctx, bson.D{{}}, findOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var book Book
+		if err := cursor.Decode(&book); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Title: %s\nAuthor: %s\n", book.Title, book.Author)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Books")
+
+}
+
+func useRoute(router *mux.Router) {
+	router.HandleFunc("/", homepage)
+	router.HandleFunc("/books", getBooks).Methods("GET")
+}
+
 func main() {
 
-	client := connectDatabase()
-	fmt.Println(client)
+	router := mux.NewRouter()
+
+	useRoute(router)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
